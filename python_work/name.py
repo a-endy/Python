1c27c3ba90690d5227f4e62b1be19277dbb3819d
a-endy <131208124+a-endy@users.noreply.github.com>
2024-01-06 16:07:40 -0500

    first commit

---
+++ python_work/name.py
@@ -0,0 +1,22 @@
+name = "ada lovelace"
+print (name.title())
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print ("Hello, " + full_name.title() + "!")
+name = "ada lovelace"
+print (name.upper())
+print (name.lower())
+print ("\tPython")
+print ("Languages:\tPython\tC\tJavaScript")
+name = 'amber endy'
+print (name)
+print ("Hi, " + name.title() + ". " + "Ready to code?")
+print (name.lower())
+print (name.upper())
+famous_person = "big yoda"
+print (famous_person.title())
+print (famous_person.title () + " once said, 'Do or not do, there is no try' ")
+print ("meh")
+help(str)
+help(list)
